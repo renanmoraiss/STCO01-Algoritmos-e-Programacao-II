ff92a583965aca1cc7595f9febc793f4f711b099
renanmoraiss <renanmorais1221@gmail.com>
2026-05-03 07:35:55 -0300

    commit

--- Merge Sort/estrutura.c
+++ Merge Sort/estrutura.c
@@ -1,44 +1,43 @@
-//Moodle
 #include <stdio.h>
 
 void merge(int A[], int inicio, int metade, int fim);
 void mergeSort(int A[], int inicio, int fim);
 
 int main() {
-    int A[] = {10, -3, 7, 0, -1, 5, 2};
-    size_t N = sizeof(A) / sizeof(A[0]);
-    mergeSort(A, 0, 6);
+    int V[] = {5, 1, 3, 2, 7};
+    size_t N = sizeof(V) / sizeof(V[0]);
+    mergeSort(V, 0, 4);
     for (int i = 0; i < N; i++) {
-        printf("%d ", A[i]);
+        printf("%d ", V[i]);
     }
     return 0;
 }
 
 void merge(int A[], int inicio, int metade, int fim) {
-    int i, j, l;
-    int n1 = metade - inicio + 1;
-    int n2 = fim - metade;
-    int esq[n1];
-    int dir[n2];
-    for (i = 0; i < n1; i++) {
+    int i, j, k;
+    int l = metade - inicio + 1;
+    int r = fim - metade;
+    int esq[l];
+    int dir[r];
+    for (i = 0; i < l; i++) {
         esq[i] = A[inicio + i];
     }
-    for (j = 0; j < n2; j++) {
+    for (j = 0; j < r; j++) {
         dir[j] = A[metade + j + 1];
     }
-    i = 0, j = 0, l = inicio;
-    while (i < n1 && j < n2) {
+    i = 0, j = 0, k = inicio;
+    while (i < l && j < r) {
         if (esq[i] <= dir[j]) {
-            A[l++] = esq[i++];
+            A[k++] = esq[i++];
         } else {
-            A[l++] = dir[j++];
+            A[k++] = dir[j++];
         }
     }
-    while (i < n1) {
-        A[l++] = esq[i++];
+    while (i < l) {
+        A[k++] = esq[i++];
     }
-    while (j < n2) {
-        A[l++] = dir[j++];
+    while (j < r) {
+        A[k++] = dir[j++];
     }
 }
 
